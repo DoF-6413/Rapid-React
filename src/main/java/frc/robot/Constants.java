99fc20c10266910076dbaa9d7c326dbe76b3325a
Liz <BestLemon555@gmail.com>
2022-04-05 18:44:58 -0700

    WIP Organizing Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,45 +5,48 @@
 package frc.robot;
 
 public final class Constants {
-    // defining joystick ports
-    final public static int initialJoystickPort = 0;
-    final public static int secondaryJoystickPort = 1;
-    // defining xbox port
-    final public static int xboxPort = 2;
+    //Ports
+
+        // defining joystick ports
+        final public static int initialJoystickPort = 0;
+        final public static int secondaryJoystickPort = 1;
+
+        // defining xbox port
+        final public static int xboxPort = 2;
+
     // defining axis
     final public static int joystickYAxis = 1;
     final public static int joystickXAxis = 0;
 
     // Defining CANIDs
-    final public static int[] leftDeviceID = new int[] { 1, 2, 3 }; // CAN ID configured using Spark MAX Client
-    final public static int[] rightDeviceID = new int[] { 4, 5, 6 };
+        //Drivetrain CAN IDs
+        final public static int[] leftDeviceID = new int[] { 1, 2, 3 }; // CAN ID configured using Spark MAX Client
+        final public static int[] rightDeviceID = new int[] { 4, 5, 6 };
 
-    final public static int[] intakeDeviceID = new int[] { 8, 9, 10 }; // Intake Motors, Number 8 and 10 for Arm, 9 for
-                                                                       // IntakeSpin
+        //Intake CAN IDs
+        final public static int[] intakeDeviceID = new int[] { 8, 9, 10 }; 
+                                                                       
+        //Shooter CAN IDs
+        final public static int[] shooterID = new int[] { 7, 11 };
 
-    final public static int[] shooterID = new int[] { 7, 11 };
-    final public static int IndexerID = 12;
-    final public static int ClimberID = 13;
+        //Indexer CAN ID
+        final public static int IndexerID = 12;
+        
+        //Climber CAN ID
+        final public static int ClimberID = 13;
 
-    // Assigned to Left Joystick Trigger
-    final public static int intakeTrigger = 1;
 
-    // Assigned to Right Joystick Trigger
-    final public static int shooterButton = 1;
-
-    // Sets Intake Speed (Currently 50%)
-    final public static double intakeSpeed = 0.5;
+    //Intake Constants
+        // Sets Intake Forward (To Shooter) Speed (Currently 50%)
+        final public static double intakeSpeed = 0.5;
+        
     final public static double reverseIntakeSpeed = -0.5;
     final public static double slowSpeed = 0.50;
-    final public static double slowSpeed2 = 0.1;
 
     // FACTS
     final public static double wheelDiameter = 4.;
     final public static double tick2feet = 1.0 / 42. * wheelDiameter * 6.11 * Math.PI / 12.;
     // final public static double FalconTurnsPerRotation = 3600;
-
-    // Sets shooter speed (Currently not used)
-    final public static double shooterSpeed = .90;
 
     // Sets indexer speed (Currently 20%)
     final public static double indexerSpeed = .60;
@@ -65,9 +68,6 @@ public final class Constants {
     // units in feet
     final public static double moveDistanceFromTarmac = 5;
 
-    final public static double lowerHubSpeed = 2300;
-    final public static double upperHubSpeed = 4000;
-
     // shooter run times seconds
     final public static double waitTimeForShooter = 15; 
     final public static double oneSecond = 1;
@@ -87,5 +87,10 @@ public final class Constants {
     // final public static double oneEightyTurn = circOfWheelToWheel/2;
     // final public static double ninetyTurn = circOfWheelToWheel/4;
     // final public static double fortyFiveTurn = circOfWheelToWheel/8;
+
+    //Shooter Speeds
+    final public static double lowerHubSpeed = 2300;
+    final public static double upperHubSpeed = 4000;
+
 
 }
