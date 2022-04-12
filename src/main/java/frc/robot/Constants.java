fbbee35eac11ba07082cab370038130f764bd5f1
Liz <BestLemon555@gmail.com>
2022-04-11 17:04:37 -0700

    Cleaned up Constants and Added Intake Actuator PID Controlls (Not Tuned)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,69 +37,74 @@ public final class Constants {
 
 
     //Intake Constants
-        // Sets Intake Forward (To Shooter) Speed (Currently 50%)
-        final public static double intakeSpeed = 0.5;
+        //Intake Speeds
+            // Sets Intake Forward (To Shooter) Speed (Currently 50%)
+            final public static double intakeSpeed = 0.5;
+            // Sets Intake Backwards (Away from Shooter) Speed (Currently 50%)
+            final public static double reverseIntakeSpeed = -0.5;
+            // Sets Intake Actuator Speed
+            final public static double slowSpeed = 0.50;
+    
+        //Intake PID (Only Used When Climbing)
+        public static final double K_IntakeActuator_P = 0.8;
+        public static final double K_IntakeActuator_I = 0.55;
+        public static final double K_IntakeActuator_D = 0;
+        public static final double K_TiltToleranceDeg = 2;
+        public static final double K_TiltRateToleranceDegPerSec = 2;
+      
+    //Indexer Constants
+        // Sets indexer speed (Currently 60%)
+        final public static double indexerSpeed = .60;
+        // indexer wait time to optimize shooter control in teleop
+        final public static double indexerWaitTime = 0.25;
+
+    //Shooter Contants
+        //Shooter PID
+        final public static double kP = 0.5;
+        final public static double kI = 15;
+        final public static double kD = 45;
+        final public static double kShooterTolerance = 1.0;
+        final public static double kShooterDerivativeTolerance = 0.01;
+        //Shooter Speeds
+        final public static double lowerHubSpeed = 2300;
+        final public static double upperHubSpeed = 4000;
         
-    final public static double reverseIntakeSpeed = -0.5;
-    final public static double slowSpeed = 0.50;
+    //Gyro Contants
+        final public static boolean K_GYRO_REVERSED = false;
+    
+    //Drivetrain Constants
+        //Drivetrain PID (Only used for turning in autonomous)
+        public static final double K_CHASSIS_TURN_P = 0.8;
+        public static final double K_CHASSIS_TURN_I = 0.55;
+        public static final double K_CHASSIS_TURN_D = 0;
+        public static final double K_TURN_TOLERANCE_DEG = 2;
+        public static final double K_TURN_RATE_TOLERANCE_DEG_PER_SEC = 2;
 
-    // FACTS
-    final public static double wheelDiameter = 4.;
-    final public static double tick2feet = 1.0 / 42. * wheelDiameter * 6.11 * Math.PI / 12.;
-    // final public static double FalconTurnsPerRotation = 3600;
-
-    // Sets indexer speed (Currently 20%)
-    final public static double indexerSpeed = .60;
-// switched to positive march 11
-    // Falcon 500 RPM Equation
-    final public static double velocityToRPM(double velocity) {
-        return velocity * 600 / 2048;
-    }
-
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SHOOTER
-    // CONSTANTS~~~~~~~~~~~~~~~~~~~~~~~~~
-    final public static double kP = 0.5;
-    final public static double kI = 15;
-    final public static double kD = 45;
-
-    final public static double kShooterTolerance = 1.0;
-    final public static double kShooterDerivativeTolerance = 0.01;
-    // auto values
-    // units in feet
-    final public static double moveDistanceFromTarmac = 5;
-
-    // shooter run times seconds
-    final public static double waitTimeForShooter = 15; 
-    final public static double oneSecond = 1;
-    final public static double twoSeconds = 2;
-    final public static double twoAndHalfSeconds = 2.5;
-    final public static double threeSeconds = 3;
-    final public static double fourSeconds = 4;
-
-    // indexer wait time to optimize shooter control in teleop
-    final public static double indexerWaitTime = 0.25;
-    //MATH
-    final public static double wheelToWheel = 26;
-    final public static double turningRadius = wheelToWheel/2;
-    final public static double turningSomething = wheelToWheel * 0.01745329;
-    final public static double circOfWheelToWheel = wheelToWheel/12 /2 * 2 * Math.PI;
-
-    // final public static double oneEightyTurn = circOfWheelToWheel/2;
-    // final public static double ninetyTurn = circOfWheelToWheel/4;
-    // final public static double fortyFiveTurn = circOfWheelToWheel/8;
-
-    //Shooter Speeds
-    final public static double lowerHubSpeed = 2300;
-    final public static double upperHubSpeed = 4000;
+    //Auto Values
+        // shooter run times in seconds
+        final public static double waitTimeForShooter = 15; 
+        final public static double oneSecond = 1;
+        final public static double twoSeconds = 2;
+            final public static double twoAndHalfSeconds = 2.5;
+            final public static double threeSeconds = 3;
+            final public static double fourSeconds = 4;
+        
+        // FACTS
+            //Drivetrain Wheel Diameter
+            final public static double wheelDiameter = 4;
+            //Drivetrain Distance in Feet Calculation
+            final public static double tick2feet = 1.0 / 42. * wheelDiameter * 6.11 * Math.PI / 12.;
+            // Falcon 500 RPM Equation
+            final public static double velocityToRPM(double velocity) {
+                return velocity * 600 / 2048;
+            }
+        //MATH FOR TURNING
+            //Wheel to Wheel Distance (Mid Left Side to Mid Right Side)
+            final public static double wheelToWheel = 26;
+            final public static double turningRadius = wheelToWheel/2;
+            final public static double turningSomething = wheelToWheel * 0.01745329;
+            final public static double circOfWheelToWheel = wheelToWheel/12 /2 * 2 * Math.PI;
 
 
 
-    final public static boolean K_GYRO_REVERSED = false;
-
-    /* Drivetrain PID */
-  public static final double K_CHASSIS_TURN_P = 0.8;
-  public static final double K_CHASSIS_TURN_I = 0.55;
-  public static final double K_CHASSIS_TURN_D = 0;
-  public static final double K_TURN_TOLERANCE_DEG = 2;
-  public static final double K_TURN_RATE_TOLERANCE_DEG_PER_SEC = 2;
 }
