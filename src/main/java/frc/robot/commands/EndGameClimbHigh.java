ce3730e3e14c381e6daf2cece3ee1ed4cdf6ac7f
El1zabethS <BestLemon555@gmail.com>
2022-04-13 19:11:23 -0700

    Added hypothetical high bar climb routine

---
+++ src/main/java/frc/robot/commands/EndGameClimbHigh.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class EndGameClimbHigh extends SequentialCommandGroup {
+  /** Creates a new EndGameClimbHigh. */
+  public EndGameClimbHigh() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new TiltTo(-30),
+      new WaitCommand(1),
+      new ClimberGoTo(45),
+      new WaitCommand(2),
+      //caution SKETCH
+      new ClimberGoTo(40),
+      parallel(new ClimberGoTo(25), new IntakeGoTo(-21.45))      
+    );
+  }
+}
