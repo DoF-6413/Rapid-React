ac119084632c2f7ee3cf7405679d1fb62ccfa916
El1zabethS <Bestlemon555@gmail.com>
2022-04-19 11:39:25 -0700

    Changed Actuator Down Auto Command to call setAllActuatorsDown instead of both individually

--- src/main/java/frc/robot/commands/ActuatorDown.java
+++ src/main/java/frc/robot/commands/ActuatorDown.java
@@ -27,8 +27,8 @@ public class ActuatorDown extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intakeSubsystem.setRightActuatorDown(0.4);
-    m_intakeSubsystem.setLeftActuatorDown(0.4);
+    m_intakeSubsystem.setAllActuatorsDown(0.4);
+    
   }
   
   // Called once the command ends or is interrupted.
